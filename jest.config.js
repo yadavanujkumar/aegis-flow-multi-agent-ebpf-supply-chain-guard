bfbd9420a6c219a4e80a13c6f86837727384c70f
dude <86741869+yadavanujkumar@users.noreply.github.com>
2026-03-06 13:08:12 +0530

    feat: add jest.config.js (GitCouncil AI)

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests/'],
+  moduleFileExtensions: ['ts', 'js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+};
